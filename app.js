84192c4d5d8a7c8c44cb59830fa78a895dafa2e6
Reinapepeada <abiarrieta@uade.edu.ar>
2022-08-15 15:32:36 -0300

    pagina original

---
+++ app.js
@@ -0,0 +1,17 @@
+const express=require("express");
+const app=express();
+const path=require("path")
+
+app.listen(3030,()=>{
+    console.log("El server esta arriba en el puerto 3030")
+})
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname,"./views/home.html"))
+})
+app.get("/login",(req,res)=>{
+    res.sendFile(path.join(__dirname,"./views/login.html"))
+})
+app.get("/register",(req,res)=>{
+    res.sendFile(path.join(__dirname,"./views/register.html"))
+})
+app.use(express.static('public'));
