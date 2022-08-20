cb294ad0c1d12c75c59d1ee38d337f9527732e06
Reinapepeada <abiarrieta@uade.edu.ar>
2022-08-20 00:53:27 -0300

    ultimo deploy

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app=express();
 const path=require("path")
 
 app.listen(process.env.PORT || 3000,()=>{
-    console.log("El server esta arriba en el puerto 3030")
+    console.log("El server esta arriba en el puerto 3000")
 })
 app.get("/",(req,res)=>{
     res.sendFile(path.join(__dirname,"./views/home.html"))
